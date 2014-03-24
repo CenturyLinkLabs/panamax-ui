de2688eca93b2fc23dbc835b261cdd92cd83f965
Alex Welch <me@alexwelch.com>
2014-03-24 14:02:02 -0600

    initial rails app [#68131316]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PanamaxUi::Application.config.secret_key_base = '5294b57ad896b834e269d5aec3141b129f3f4d27a5072e8acae2a6915b20d13c622b942360eaaf92012907227f98f911d6d795c47c0e5a228073a130e98f15e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PanamaxUi::Application.config.session_store :cookie_store, key: '_panamax-ui_session'
