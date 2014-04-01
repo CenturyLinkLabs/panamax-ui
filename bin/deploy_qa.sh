b98e6688e1c63f8c10a63a47bc3a1b85f4b4cbf8
Pat Cox <patrick.cox@savvis.com>
2014-04-01 08:29:42 -0500

    Typo in deploy script file

--- bin/deploy_qa.sh
+++ bin/deploy_qa.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh clouduser@63.128.180.11 "cd /home/panamax && git pull && ./setup.sh -op=reinstall && cd ~ && ./screensh.sh"
+ssh clouduser@63.128.180.11 "cd /panamax-vagrant && git pull && ./setup.sh -op=reinstall && cd ~ && ./screensh.sh"
