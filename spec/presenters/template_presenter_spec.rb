70b1df95e97bbba9ab89e1f67046c4cdc0261d0c
Alex <me@alexwelch.com>
2014-04-07 16:56:48 -0600

    Differentiate between templates and images. [fixes #68586576]

--- spec/presenters/template_presenter_spec.rb
+++ spec/presenters/template_presenter_spec.rb
@@ -1,30 +1,30 @@
 require 'spec_helper'
 
-describe ImagePresenter do
-  let(:fake_image) do
-    double(:fake_image, {
-      repository: 'boom/shaka',
+describe TemplatePresenter do
+  let(:fake_template) do
+    double(:fake_template, {
+      name: 'boom shaka',
       description: 'goes boom shaka laka',
       updated_at: 'Mon'
     })
   end
 
-  subject { ImagePresenter.new(fake_image) }
+  subject { TemplatePresenter.new(fake_template) }
 
   describe '#title' do
-    it 'exposes the image repository' do
-      expect(subject.title).to eq 'boom/shaka'
+    it 'exposes the template name' do
+      expect(subject.title).to eq 'boom shaka'
     end
   end
 
   describe '#description' do
-    it 'exposes the image description' do
+    it 'exposes the template description' do
       expect(subject.description).to eq 'goes boom shaka laka'
     end
   end
 
   describe '#updated_at' do
-    it 'exposes the image updated_at' do
+    it 'exposes the template updated_at' do
       expect(subject.updated_at).to eq 'Mon'
     end
   end
