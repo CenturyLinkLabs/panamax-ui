25bd13575bda01821e4e8f15cd2be9e731753634
Pat Cox <patrick.cox@savvis.com>
2014-04-09 15:27:52 -0500

    Edited deploy script to add local image [ci skip]

--- bin/deploy_qa.sh
+++ bin/deploy_qa.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh clouduser@63.128.180.11 "cd panamax-vagrant && git pull && ./setup.sh -op=reinstall && vagrant ssh && docker push 74.201.240.198:5000/panamax-ui && docker push 74.201.240.198:5000/panamax-api && exit && cd ~ && ./screensh.sh"
+ssh clouduser@63.128.180.11 "cd panamax-vagrant && git pull && ./setup.sh -op=reinstall && vagrant ssh && docker push 74.201.240.198:5000/panamax-ui && docker push 74.201.240.198:5000/panamax-api && docker pull lapax/tiny-haproxy && exit && cd ~ && ./screensh.sh"
