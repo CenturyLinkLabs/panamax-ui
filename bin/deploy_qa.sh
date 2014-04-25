ce38124c0ccee25412c56758629aa92ca7eefcb7
Pat Cox <patrick.cox@savvis.com>
2014-04-25 14:29:27 -0500

    Changed deploy script back to use reinstall [ci skip]

--- bin/deploy_qa.sh
+++ bin/deploy_qa.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh clouduser@63.128.180.11 "cd panamax-vagrant; git pull; vagrant destroy -f; ./setup.sh -op=install; cd ~; ./commit.sh; ./screensh.sh"
+ssh clouduser@63.128.180.11 "cd panamax-vagrant; git pull; ./setup.sh -op=reinstall; cd ~; ./commit.sh; ./screensh.sh"
