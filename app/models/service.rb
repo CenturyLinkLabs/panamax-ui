dceb850e88d66c786d82b6fc3b51dedd9c9d95c1
Mike Arnold <dharmamike@gmail.com>
2014-05-01 11:00:54 -0500

    building nested model object graph

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,11 +3,16 @@ require 'active_model'
 class Service
   include ActiveModel::Model
 
-  attr_reader :name, :id
+  attr_reader :name, :id, :categories
 
   def initialize(attributes={})
     @name = attributes['name']
     @id = attributes['id']
+    @categories = attributes['categories']
+  end
+
+  def category_names
+    categories.map(&:name)
   end
 
   def to_param
@@ -16,6 +21,11 @@ class Service
 
   def self.create_from_response(response)
     attributes = JSON.parse(response)
+    create_with_sub_resources(attributes)
+  end
+
+  def self.create_with_sub_resources(attributes)
+    attributes['categories'] = attributes['categories'].map{ |category_hash| ServiceCategory.new(category_hash)  }
     self.new(attributes)
   end
 
