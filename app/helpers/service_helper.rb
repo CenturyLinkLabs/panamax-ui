c4d70fdc83599ef99b169f510764b627452a4f8c
Alex Welch <me@alexwelch.com>
2014-05-16 11:50:45 -0600

    Pass the block through the add_fields_for helper.

--- app/helpers/service_helper.rb
+++ app/helpers/service_helper.rb
@@ -1,8 +1,7 @@
 module ServiceHelper
-  def add_fields_for(name, f, options={})
+  def add_fields_for(name, f, options={}, &block)
     new_object = OpenStruct.new(persisted?: false)
-    f.fields_for(name, new_object, options) do |builder|
-      render("new_#{name}_fields", f: builder)
-    end
+    f.fields_for(name, new_object, options, &block)
   end
+
 end
