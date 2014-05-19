2c23b093eb2533593139e9c3dad8f45907cb1d50
Gary Paige <gary@theoryblister.com>
2014-05-19 11:45:58 -0500

    adds relations slide view to manage application [finishes: #70053874]

---
+++ spec/javascripts/jquery.services_views_spec.js
@@ -0,0 +1,38 @@
+describe('$.fn.serviceViews', function () {
+  beforeEach(function () {
+    fixture.load('service-view-toggle.html');
+    $('section.application-services').serviceViews();
+    $listView = $('#teaspoon-fixtures').find('.list');
+    $relationshipView = $('#teaspoon-fixtures').find('.relationship');
+  });
+
+  describe('clicking list view', function() {
+    it('adds .selected class', function() {
+      expect($listView.hasClass('selected')).toBeFalsy();
+      $listView.click();
+      console.log("a: ", $listView);
+      expect($listView.hasClass('selected')).toBeTruthy();
+    });
+
+    it('removes .selected class from other links', function() {
+      $relationshipView.addClass('selected');
+      $listView .click();
+      expect($relationshipView.hasClass('selected')).toBeFalsy();
+    });
+  });
+
+  describe('clicking relationship view', function() {
+    it('adds .selected class', function() {
+      expect($relationshipView.hasClass('selected')).toBeFalsy();
+      $relationshipView.click();
+      expect($relationshipView.hasClass('selected')).toBeTruthy();
+    });
+
+    it('removes .selected class from other links', function() {
+      $listView.addClass('selected');
+      $relationshipView.click();
+      expect($listView.hasClass('selected')).toBeFalsy();
+    });
+  });
+
+});
