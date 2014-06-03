5fbeee3049f063090b676203cdf504b142797029
Rupak Ganguly <rupakg@gmail.com>
2014-06-03 12:49:59 -0400

    Add User model and update template to show a url to generate access token if not present.
    Signed-off-by: Alex Welch <me@alexwelch.com>

--- spec/features/manage_templates_spec.rb
+++ spec/features/manage_templates_spec.rb
@@ -1,14 +1,35 @@
 require 'spec_helper'
 
 describe 'managing a template' do
+
   context 'as a user' do
+
     context 'from a running application' do
-      it 'create a template' do
+
+      it 'creates a template' do
         visit '/applications/2'
 
         click_on 'Save as Template'
 
-        page.should have_css 'h1', text: 'Save as Template'
+        expect(page).to have_css 'h1', text: 'Save as Template'
+
+        expect(page).not_to have_content 'Generate a Github access token'
+
+        expect(page).to have_content 'We haz your Github access token!'
+      end
+
+      context 'when user does not have a github access token' do
+
+        it 'has a link to generate a new github token' do
+          User.stub(:find).and_return(double(:user, github_access_token_present?: false))
+
+          visit '/templates/new'
+
+          expect(page).to have_link(
+            'Generate a Github access token',
+            href: 'https://github.com/settings/tokens/new?scope=repo,user:email'
+          )
+        end
       end
     end
   end
