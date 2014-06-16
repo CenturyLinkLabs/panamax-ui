f224e5865de947df9d217499c00df9e297b11b2c
Alex Welch <me@alexwelch.com>
2014-06-16 16:35:54 -0600

    delegate to instance vars instead of private readers

--- app/presenters/service_row_presenter.rb
+++ app/presenters/service_row_presenter.rb
@@ -1,16 +1,12 @@
 class ServiceRowPresenter
 
-  delegate :icon, :name, :status, to: :service
+  delegate :icon, :name, :status, to: :@service
 
   def initialize(service)
     @service = service
   end
 
   def service_url
-    "/applications/#{service.app.id}/services/#{service.id}"
+    "/applications/#{@service.app.id}/services/#{@service.id}"
   end
-
-  private
-
-  attr_reader :service
 end
