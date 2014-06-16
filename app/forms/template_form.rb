b14e9cc42f4bf78651c98b76c0ef9d80baffb6bc
Alex Welch <me@alexwelch.com>
2014-06-16 14:16:22 -0600

    Adds ability to give template a description. [#72098640]

--- app/forms/template_form.rb
+++ app/forms/template_form.rb
@@ -3,11 +3,14 @@ require 'active_model'
 class TemplateForm
   include ActiveModel::Model
 
-  attr_accessor :repos, :name
+  attr_accessor :repos, :name, :description
 
   def save
     if valid?
-      Template.create(name: name)
+      Template.create(
+        name: name,
+        description: description
+      )
     end
   end
 end
