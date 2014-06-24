50778b530dd015192c69efdc040dd27516c0da7e
Alex Welch <me@alexwelch.com>
2014-06-24 11:25:38 -0600

    Add author to template form ...and default it to user's email addy. [#73547476]

--- app/forms/template_form.rb
+++ app/forms/template_form.rb
@@ -4,13 +4,19 @@ class TemplateForm
   include ActiveModel::Model
 
   attr_accessor :repos, :name, :description, :keywords
+  attr_writer :author, :user
+
+  def author
+    @author || @user.try(:email)
+  end
 
   def save
     if valid?
       Template.create(
         name: name,
         description: description,
-        keywords: keywords
+        keywords: keywords,
+        authors: [author]
       )
     end
   end
