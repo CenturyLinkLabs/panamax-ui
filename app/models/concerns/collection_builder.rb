edd5b876b8f6911e4769b46a2aafa716c48f3f22
Brian DeHamer <brian@dehamer.com>
2014-06-26 12:41:23 -0700

    Remove dead code

--- app/models/concerns/collection_builder.rb
+++
@@ -1,9 +0,0 @@
-module CollectionBuilder
-  extend ActiveSupport::Concern
-
-  module ClassMethods
-    def instantiate_collection(hashes)
-      Array(hashes).map { |hash| self.new(hash) }
-    end
-  end
-end
