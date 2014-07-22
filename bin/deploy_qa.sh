7c81521d68913beba4f3b8e1bd6913e373be3314
patocox <patrickocox@gmail.com>
2014-07-22 11:22:01 -0500

    Update to new QA server

--- bin/deploy_qa.sh
+++ bin/deploy_qa.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh clouduser@63.128.180.11 "cd panamax-vagrant; git pull; ./setup.sh -op=reinstall; cd ~; ./commit.sh; ./screensh.sh"
+ssh root@8.22.8.121 "cd panamax-vagrant; git pull; ./setup.sh -op=reinstall; cd ~; ./commit.sh; ./screensh.sh"
