09ac9536018df45cf0cde1035999dcc40de17ddc
Laura Frank <ljfrank@gmail.com>
2014-07-28 08:53:08 -0500

    How it works interaction [ci-skip]

--- lucas/assets/javascripts/index.js
+++ lucas/assets/javascripts/index.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 
+  //How It Works
+
+  var $header = $('#how-it-works h3');
+  $header.click(function (e) {
+
+    var sectionName = e.target.id;
+
+    $('#how-it-works li').removeClass('active');
+    $(this).parent().toggleClass('active');
+    $('#how-it-works').attr("class", sectionName);
+
+  });
 });
