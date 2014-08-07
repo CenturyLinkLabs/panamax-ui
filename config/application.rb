cbfd606492190db89d132b1201d2d50e3fc798dd
Alex Welch <me@alexwelch.com>
2014-08-07 16:11:58 -0600

    set the exception app

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module PanamaxUi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.exceptions_app = self.routes
   end
 end
