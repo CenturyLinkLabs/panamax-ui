2f6bee1f5ebdcc639d4647d4f1bf7901c1d1cabe
Gary Paige <gary@theoryblister.com>
2014-10-23 06:32:53 -0500

    tests and changelog

--- app/assets/javascripts/jquery.finger_tabs.js
+++ app/assets/javascripts/jquery.finger_tabs.js
@@ -7,9 +7,13 @@
     base.defaultOptions = {
       cardSelector: '.card',
       tabSelector: '.tab',
+      activeIconSelector: '.tab.active .icon',
       tabsSelector: '.tabs',
       hideSelector: '.hide',
-      labelSelector: 'label'
+      labelSelector: 'label',
+      updateFormEvent: { event: 'progressiveForm:changed',
+                         target: 'body'
+                       }
     };
 
     base.init = function () {
@@ -25,6 +29,7 @@
     base.bindEvents = function() {
       base.$el.on('click', base.options.tabSelector, base.tabClickHandler);
       base.$el.on('click', base.options.hideSelector, base.hideHandler);
+      $(base.options.updateFormEvent.target).on(base.options.updateFormEvent.event, base.dataChangedHandler);
     };
 
     base.hideHandler = function(e) {
@@ -33,22 +38,25 @@
 
     base.selectTab = function($tab_element) {
       var tab_for = $tab_element.find(base.options.labelSelector).attr('for');
-      console.log(tab_for);
       base.resetTabs();
       $tab_element.addClass('active');
-      base.$el.find(base.options.cardSelector + '.' + tab_for).css('display', 'block');
+      base.$el.find(base.options.cardSelector + '.' + tab_for).addClass('active');
 
     };
 
     base.resetTabs = function() {
-      base.$el.find(base.options.cardSelector).css('display', 'none');
+      base.$el.find(base.options.cardSelector).removeClass('active');
       base.$el.find(base.options.tabSelector).removeClass('active');
     };
 
     base.tabClickHandler = function(e) {
       var $target = $(e.currentTarget);
       base.selectTab($target);
-    }
+    };
+
+    base.dataChangedHandler = function(e) {
+      base.$el.find(base.options.activeIconSelector).addClass('changed');
+    };
   };
 
   $.fn.fingerTabs = function(options){
