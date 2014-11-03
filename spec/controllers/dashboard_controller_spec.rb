9eb1fccd9901d881dc6e1881b62ce93631b47dd2
Gary Paige <gary@theoryblister.com>
2014-11-02 21:25:55 -0600

    updated layout for dashboard view

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,43 +1,45 @@
 require 'spec_helper'
 
 describe DashboardController do
+  let(:response) { double('response', response: []) }
+  let(:resources) do
+    {
+        'Application' => { count: 1, manage_path: apps_path, title: 'test', message: 'test' },
+        'Source' => { count: 1, manage_path: template_repos_path, title: 'test', message: 'test'  },
+        'Image' => { count: 1, manage_path: images_path, title: 'test', message: 'test'  },
+        'Registry' => { count: 1, manage_path: registries_path, title: 'test', message: 'test'  },
+        'DeploymentTarget' => { count: 1, manage_path: deployment_targets_path, title: 'test', message: 'test'  }
+    }
+  end
+
+  before do
+    I18n.stub(:t).and_return('test')
+  end
 
   describe 'GET #index' do
-    let(:limit) { 5 }
-    let(:response) { double('response', header: { 'Total-Count' => '10' }) }
-    let(:resource_response) { double('resource_response', response: response) }
-    let(:resources) do
-      {
-        'Application' => { list: resource_response, count: 10, manage_path: apps_path, more_count: 5 },
-        'Source' => { list: resource_response, count: 10, manage_path: template_repos_path, more_count: 5  },
-        'Image' => { list: resource_response, count: 10, manage_path: images_path, more_count: 5  },
-        'Registry' => { list: resource_response, count: 10, manage_path: registries_path, more_count: 5  }
-      }
-    end
-
     it 'calls all_with_response on APP' do
-      expect(App).to receive(:all_with_response).with(params: { limit: limit }).and_return(resource_response)
+      expect(App).to receive(:all)
       get :index
     end
 
     it 'calls all_with_response on TemplateRepo' do
-      expect(TemplateRepo).to receive(:all_with_response).with(params: { limit: limit }).and_return(resource_response)
+      expect(TemplateRepo).to receive(:all)
       get :index
     end
 
     it 'calls all_with_response on LocalImage' do
-      expect(LocalImage).to receive(:all_with_response).with(params: { limit: limit }).and_return(resource_response)
+      expect(LocalImage).to receive(:all)
       get :index
     end
 
     it 'calls all_with_response on Registry' do
-      expect(Registry).to receive(:all_with_response).with(params: { limit: limit }).and_return(resource_response)
+      expect(Registry).to receive(:all)
       get :index
     end
 
     it 'assigns values to the resources instance variable' do
-      [App, TemplateRepo, LocalImage, Registry].each do |resource|
-        resource.stub(:all_with_response).and_return(resource_response)
+      [App, TemplateRepo, LocalImage, Registry, DeploymentTarget].each do |resource|
+        resource.stub(:all).and_return(['thing'])
       end
 
       get :index
@@ -46,7 +48,7 @@ describe DashboardController do
 
     it 'the map of resources contains expected keys' do
       get :index
-      expect(assigns(:resources).keys).to match_array %w(Application Source Image Registry)
+      expect(assigns(:resources).keys).to match_array %w(Application Source Image Registry DeploymentTarget)
     end
   end
 end
--- spec/features/manage_dashboard_spec.rb
+++ spec/features/manage_dashboard_spec.rb
@@ -10,48 +10,37 @@ describe 'manage dashboard' do
 
     it 'can link to manage applications' do
       visit '/dashboard'
-      click_on 'Manage Applications'
+      find('a', text: 'Manage your Applications').click
 
       expect(page).to have_css 'li', text: 'My Applications'
     end
 
     it 'can link to manage sources' do
       visit '/dashboard'
-      click_on 'Manage Sources'
+      find('a', text: 'Manage your Sources').click
 
       expect(page).to have_css 'li', text: 'Sources'
     end
 
     it 'can link to manage images' do
       visit '/dashboard'
-      click_on 'Manage Images'
+      find('a', text: 'Manage your Images').click
 
       expect(page).to have_css 'li', text: 'Images'
     end
 
-    it 'can link to manage registries' do
+    it 'can link to remote deployment targets images' do
       visit '/dashboard'
-      click_on 'Manage Registries'
+      find('a', text: 'Manage your Targets').click
+
+      expect(page).to have_css 'li', text: 'Remote Deployment Targets'
+    end
+
+    it 'can link to registries' do
+      visit '/dashboard'
+      find('a', text: 'Manage your Registries').click
 
       expect(page).to have_css 'li', text: 'Registries'
-
-      expect(page).to have_content 'my_top_secret_registry'
-      expect(page).to have_content 'localhost:5000'
-    end
-
-
-    it 'can link to applications' do
-      visit '/dashboard'
-      click_on 'tutum/wordpress'
-
-      expect(page).to have_content 'Deployed to'
-      expect(page).to have_css 'li', text: 'tutum/wordpress'
-    end
-
-    it 'can link to sources' do
-      visit '/dashboard'
-
-      expect(page).to have_link 'ctllabs/canonical'
     end
   end
 end
