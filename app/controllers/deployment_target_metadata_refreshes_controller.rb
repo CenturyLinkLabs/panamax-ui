13d35bc5a2c1fc93f2d84c91bc317ac46a9620b4
Don Petersen <don@donpetersen.net>
2014-11-17 22:18:43 +0000

    Add JSON response to metadata refresh endpoint.
    This will be used in a target health check on deployment.

--- app/controllers/deployment_target_metadata_refreshes_controller.rb
+++ app/controllers/deployment_target_metadata_refreshes_controller.rb
@@ -1,13 +1,12 @@
 class DeploymentTargetMetadataRefreshesController < ApplicationController
-  respond_to :html
+  respond_to :html, :json
 
   def create
-    deployment_target_id = params[:deployment_target_id]
-
-    DeploymentTargetMetadataRefresh.create(
-      deployment_target_id: deployment_target_id
+    refresh = DeploymentTargetMetadataRefresh.create(
+      deployment_target_id: params[:deployment_target_id]
     )
+
     flash[:success] = I18n.t('deployment_targets.metadata_refresh.success')
-    redirect_to deployment_targets_path
+    respond_with(refresh, location: deployment_targets_path)
   end
 end
--- spec/controllers/deployment_target_metadata_refreshes_controller_spec.rb
+++ spec/controllers/deployment_target_metadata_refreshes_controller_spec.rb
@@ -2,21 +2,32 @@ require 'spec_helper'
 
 describe DeploymentTargetMetadataRefreshesController do
   describe '#create' do
+    subject { response }
+    let(:format) { :html }
     before do
-      DeploymentTargetMetadataRefresh.stub(create: double)
-      post :create, deployment_target_id: "19"
+      DeploymentTargetMetadataRefresh.stub(:create).and_call_original
+      post :create, deployment_target_id: '19', format: format
     end
 
     it 'creates a DeploymentTargetMetadataRefresh with the expected DeploymentTarget' do
       expect(DeploymentTargetMetadataRefresh).to(
         have_received(:create).
-        with({ deployment_target_id: "19" })
+        with({ deployment_target_id: '19' })
       )
     end
 
     context 'HTML format' do
-      it 'redirects to the deployments list' do
-        expect(response).to redirect_to(deployment_targets_path)
+      it { should redirect_to(deployment_targets_path) }
+    end
+
+    context 'JSON format' do
+      let(:format) { :json }
+
+      its(:status) { should eq(201) }
+
+      describe 'the JSON response' do
+        subject(:hash) { JSON.parse(response.body) }
+        its(:keys) { should include('agent_version', 'adapter_type') }
       end
     end
   end
