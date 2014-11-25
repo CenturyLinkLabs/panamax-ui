3324a1cba067912136f8f6f56e450fff33ab0d95
Brian DeHamer <brian@dehamer.com>
2014-11-25 08:07:01 -0800

    WIP

--- app/controllers/deployment_target_metadata_refreshes_controller.rb
+++ app/controllers/deployment_target_metadata_refreshes_controller.rb
@@ -7,7 +7,8 @@ class DeploymentTargetMetadataRefreshesController < ApplicationController
     DeploymentTargetMetadataRefresh.create(
       deployment_target_id: deployment_target_id
     )
-    flash[:success] = I18n.t('deployment_targets.metadata_refresh.success')
+    flash[:success] = I18n.
+      t('deployment_targets.metadata_refresh.success')
     redirect_to deployment_targets_path
   end
 end
