641685af6b46faf83c69af3fba2302a12fb6e042
Mike Arnold <dharmamike@gmail.com>
2014-12-16 11:32:17 -0600

    enabling client-side features for redeployment
    Signed-off-by: Gary Paige <gary@theoryblister.com>

--- spec/javascripts/jquery.confirm_and_redeploy_spec.js
+++ spec/javascripts/jquery.confirm_and_redeploy_spec.js
@@ -1,29 +1,30 @@
-describe('$.fn.confirmAndDelete', function() {
-  var subject
+describe('$.fn.confirmAndRedeploy', function() {
+  var subject;
 
   beforeEach(function() {
-    fixture.load('confirm-and-delete.html');
-    subject = new $.PMX.ConfirmAndDelete($('body'));
+    fixture.load('confirm-and-redeploy.html');
+    subject = new $.PMX.ConfirmAndRedeploy($('body'));
     subject.init();
   });
 
   describe('when clicked', function() {
     it('prevents default', function() {
       var click = new $.Event('click');
-      $('.delete-action').trigger(click);
+      $('.redeploy').trigger(click);
       expect(click.isDefaultPrevented).toBeTruthy();
     });
 
     it('prevents propagation', function() {
       var click = new $.Event('click');
-      $('.delete-action').trigger(click);
+      $('.redeploy').trigger(click);
       expect(click.isPropagationStopped).toBeTruthy();
     });
 
     it('call createConfirmation', function() {
       var spy = spyOn($.PMX, 'ConfirmDelete').andCallThrough(),
-          click = new $.Event('click');
-      $('.delete-action').trigger(click);
+          click = new $.Event('click'),
+          $target = $('[data-action-confirm]');
+      $target.trigger(click);
       expect(spy).toHaveBeenCalled();
     });
   });
